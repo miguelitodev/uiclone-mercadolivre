9cb61923d12c66acbe259adc67ea5eb569c97f77
miguelrisquelme <miguelrisquelme@gmail.com>
2020-09-22 01:22:12 -0300

    a litle of changes to project - tomorrow I continue

--- src/components/SellerInfo/index.tsx
+++ src/components/SellerInfo/index.tsx
@@ -1,11 +1,57 @@
-import React from 'react';
+import React from "react";
 
-import { Container } from './styles';
+import {
+  Container,
+  Title,
+  LocationCard,
+  LocationIcon,
+  ReputationCard,
+  ReputationThermometer,
+  Reo,
+} from "./styles";
 
 const SellerInfo: React.FC = () => {
   return (
     <Container>
-      <h1>SellerInfo</h1>
+      <Title>Informações sobre o vendedor</Title>
+
+      <LocationCard>
+        <LocationIcon></LocationIcon>
+        <div>
+          <p>Localização</p>
+          <strong>São Paulo, São Paulo</strong>
+        </div>
+      </LocationCard>
+
+      <ReputationCard>
+        <ReputationThermometer>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li className="active"></li>
+        </ReputationThermometer>
+        <ReputationRow>
+          <div>
+            <strong>561</strong>
+            <p> vendas nos últimos 4 meses</p>
+          </div>
+          <div>
+            <strong>
+              <SupportIcon />
+            </strong>
+            <p> vendas nos últimos 4 meses</p>
+          </div>
+          <div>
+            <strong>
+              <ClockIcon />
+            </strong>
+            <p> vendas nos últimos 4 meses</p>
+          </div>
+        </ReputationRow>
+      </ReputationCard>
+
+      <More href="#">Ver mais dados do vendedor</More>
     </Container>
   );
 };
