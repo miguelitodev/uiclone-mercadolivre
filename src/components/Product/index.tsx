5e2bf3f3089720e30df10dcb7689cedd5e11a41b
miguelrisquelme <miguelrisquelme@gmail.com>
2020-09-21 23:49:51 -0300

    Add structure of two components
    added structure of WarrantySection and Info on file index.tsx from /Layout

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,13 +1,80 @@
-import React from 'react';
+import React from "react";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
 const Product: React.FC = () => {
   return (
     <Container>
-      <h1>Product</h1>
+      <Row>
+        <a href="#">Compartilhar</a>
+        <a href="#">Vender um igual</a>
+      </Row>
+
+      <Panel>
+        <Column>
+          <Gallery>
+            <img src="T-Shirt" loading="lazy" alt={tshirtImage} />
+          </Gallery>
+          <Info></Info>
+        </Column>
+
+        <Column>
+          {/* <ProductAction></ProductAction> */}
+          {/* <SellerInfo></SellerInfo> */}
+
+          <WarrantySection></WarrantySection>
+          <WarrantySection></WarrantySection>
+          <WarrantySection></WarrantySection>
+        </Column>
+      </Panel>
     </Container>
   );
 };
+
+const WarrantySection = () => (
+  <Section>
+    <h4>Garantia</h4>
+    <div>
+      <span>
+        <p className="title">Compra garantida com o Lorem Ipsum</p>
+        <p className="description">
+          Receba o produto que você está esperando, ou devolvemos o seu dinheiro
+        </p>
+      </span>
+      <span>
+        <p className="title">Garantida do vendedor</p>
+        <p className="description">Sem garantia</p>
+      </span>
+    </div>
+  </Section>
+);
+
+const Info = () => (
+  <Description>
+    <h2>Descrição</h2>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit pariatur
+      temporibus perspiciatis vero fugiat, error repellendus quod, saepe
+      necessitatibus illum voluptas, alias architecto porro facere! Cumque sed
+      ex repudiandae facilis!
+      <br />
+      <br />
+      Items inclusos:
+      <br />
+      - 1x LED
+      <br />
+      - 1x LED
+      <br />
+      - 1x LED
+      <br />
+      - 1x LED
+      <br />
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit pariatur
+      temporibus perspiciatis vero fugiat, error repellendus quod, saepe
+      necessitatibus illum voluptas, alias architecto porro facere! Cumque sed
+      ex repudiandae facilis!
+    </p>
+  </Description>
+);
 
 export default Product;
