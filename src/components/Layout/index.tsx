1a110b5f338399a4d6bb1802820303d733a754eb
miguelrisquelme <miguelrisquelme@gmail.com>
2020-09-22 22:48:53 -0300

    Add header and footer on layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 
 import Product from "./../Product";
+import Header from "./../Header";
+import Footer from "./../Footer";
 import { Container, Wrapper } from "./styles";
 
 const Layout: React.FC = () => {
   return (
     <Container>
-      {/* <Header /> */}
+      <Header />
       <Wrapper>
         <Product></Product>
       </Wrapper>
-      {/* <Footer/> */}
+      <Footer />
     </Container>
   );
 };
